a444d2337a8754b2d7e73862fafacd82d5b743ea
INGOLD <fogold07@gmail.com>
2021-12-02 11:36:41 +0100

    Exo 1 à 5 ok

--- src/test/java/java8/ex05/Function_05_Test.java
+++ src/test/java/java8/ex05/Function_05_Test.java
@@ -4,6 +4,9 @@ import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -15,15 +18,15 @@ public class Function_05_Test {
     //tag::functions[]
     // TODO compléter la fonction
     // TODO modifier le mot de passe en "secret"
-    Consumer<Person> changePasswordToSecret = null;
+    Consumer<Person> changePasswordToSecret = p -> p.setPassword("secret");  
 
     // TODO compléter la fonction
     // TODO vérifier que l'age > 4 avec une assertion JUnit
-    Consumer<Person> verifyAge = null;
+    Consumer<Person> verifyAge = p -> assertTrue(p.getAge()>4);;
 
     // TODO compléter la fonction
     // TODO vérifier que le mot de passe est "secret" avec une assertion JUnit
-    Consumer<Person> verifyPassword = null;
+    Consumer<Person> verifyPassword = p -> assertEquals("secret",p.getPassword());
     //end::functions[]
 
 
@@ -33,13 +36,16 @@ public class Function_05_Test {
 
         // TODO invoquer la méthode personList.forEach pour modifier les mots de passe en "secret"
         // personList.forEach...
-
+        personList.forEach(changePasswordToSecret);
+        
         // TODO remplacer la boucle for par l'invocation de la méthode forEach
         // TODO Utiliser la méthode andThen pour chaîner les vérifications verifyAge et verifyPassword
         // personList.forEach...
+        personList.forEach(verifyAge.andThen(verifyPassword));
+        /*
         for(Person p : personList) {
             verifyAge.accept(p);
             verifyPassword.accept(p);
-        }
+        }*/
     }
 }
